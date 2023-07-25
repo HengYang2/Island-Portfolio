ba346b729b510bd24e2efb3f17268bbba90f668c
Heng Yang <hengya03@gmail.com>
2023-07-25 17:01:32 -0500

    Added a tooltip popup that shows when you hover over the teleport icons.

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <div className='top-0 left-0 fixed min-h-full w-36 flex flex-col gap-2 justify-start items-center'>
-            <div className='top-0 left-0 absolute min-h-full min-w-full bg-gray-500 opacity-40 z-999'></div>
+        <div className='top-0 left-0 fixed min-h-full w-32 flex flex-col gap-2 justify-start items-center'>
+            <div className='top-0 left-0 absolute min-h-full min-w-full bg-black opacity-60 z-999'></div>
             <div className=' mt-2 flex flex-col gap-0 justify-center items-center z-1000 relative'>
                 <a href="" className='info-icon'>LinkedIn</a>
                 <a href="" className='info-icon'>Resume</a>
                 <a href="" className='info-icon'>Github</a>
             </div>
             <div className='flex flex-col gap-2 justify-center items-center z-1000 relative'>
-                <button className='teleport-icon'>H</button>
-                <button className='teleport-icon'>C</button>
-                <button className='teleport-icon'>Te</button>
-                <button className='teleport-icon'>Tes</button>
-                <button className='teleport-icon'>P</button>
+                <button className='teleport-icon group'> H <span className='icon-tooltip group-hover:scale-100'>About Me</span> </button>
+                <button className='teleport-icon group'> C <span className='icon-tooltip group-hover:scale-100'>Portfolio</span> </button>
+                <button className='teleport-icon group'> F <span className='icon-tooltip group-hover:scale-100'>Tech Stack</span> </button>
+                <button className='teleport-icon group'> B <span className='icon-tooltip group-hover:scale-100'>Testimonials</span> </button>
+                <button className='teleport-icon group'> P <span className='icon-tooltip group-hover:scale-100'>Get In Touch!</span> </button>
             </div>
         </div>
     )
