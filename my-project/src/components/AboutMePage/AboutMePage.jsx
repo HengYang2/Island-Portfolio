42d9432c72ea6ead62ccca820eae0a59e04d1201
Heng Yang <hengya03@gmail.com>
2023-07-26 16:43:07 -0500

    Added opacity to the dialog divs.

--- my-project/src/components/AboutMePage/AboutMePage.jsx
+++ my-project/src/components/AboutMePage/AboutMePage.jsx
@@ -11,7 +11,8 @@ export default function AboutMePage() {
     //Conditionally render question options based on the questionsVisable useState:
     function toggleQuestionsVisable() {
         return questionsVisable ?
-            <div className='bg-yellow-100 w-40 h-36 ml-90 mt-40 absolute flex flex-col justify-center gap-1 items-center border border-black rounded-md box-shadow animate-subtlePulse'>
+            <div className='w-40 h-36 ml-90 mt-40 absolute flex flex-col justify-center gap-1 items-center'>
+                <div className='bg-yellow-100 absolute w-full h-full border border-black rounded-md box-shadow animate-subtlePulse -z-1 opacity-70'></div>
                 <button className='text-option' id='option1' onClick={e => { setSelectedQuestion(`${e.target.id}`); setQuestionsVisable(false) }}>Who are you?</button>
                 <button className='text-option' id='option2' onClick={e => { setSelectedQuestion(`${e.target.id}`); setQuestionsVisable(false) }}>Where am I?</button>
                 <button className='text-option' id='option3' onClick={e => { setSelectedQuestion(`${e.target.id}`); setQuestionsVisable(false) }}>What do you do for fun?</button>
@@ -25,7 +26,7 @@ export default function AboutMePage() {
             case 'option1':
                 return <p onClick={() => { setSelectedQuestion(''); setQuestionsVisable(true) }} className='relative bg-white left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md box-shadow'>My name is Heng Yang, I'm a software developer who enjoys creating games, making music and chilling on this island.</p>
             case 'option2':
-                return <p onClick={() => { setSelectedQuestion(''); setQuestionsVisable(true) }} className='relative bg-white left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md box-shado'>You are on Paradise Island, a place created from my imagination. You are expereiencing a piece of my mind!</p>
+                return <p onClick={() => { setSelectedQuestion(''); setQuestionsVisable(true) }} className='relative bg-white left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md box-shadow'>You are on Paradise Island, a place created from my imagination. You are expereiencing a piece of my mind!</p>
             case 'option3':
                 return <p onClick={() => { setSelectedQuestion(''); setQuestionsVisable(true) }} className='relative bg-white left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md box-shadow'>I enjoy making music, 3d modeling, coding, and spending time with friends and family.</p>
             default:
@@ -53,7 +54,8 @@ export default function AboutMePage() {
     return (
         <div className='absolute top-0 left-0 w-full h-full pt-16'>
             {toggleQuestionsVisable()}
-            <div className='relative w-3/4 h-44 bg-yellow-100 ml-40 mt-80 border border-black rounded-md flex flex-row justify-center gap-2 box-shadow animate-subtlePulse'>
+            <div className='relative w-3/4 h-44 ml-40 mt-80 flex flex-row justify-center gap-2'>
+            <div className='bg-yellow-100 absolute w-full h-full border border-black rounded-md box-shadow animate-subtlePulse -z-1 opacity-70'></div>
                 <div className='left-.5 top-1.5 h-32 w-32 relative'>
                     {imageAnimationRender()}
                     <h1 className='bg-white border border-black rounded-md mt-1.5 text-center box-shadow'>Heng Yang</h1>
