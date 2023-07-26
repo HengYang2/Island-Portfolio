f38607d557ec9029ec825e8defa258fee36c2bf1
Heng Yang <hengya03@gmail.com>
2023-07-26 01:51:39 -0500

    Made the question options dissapear when you select one of them. The questions reapear after you click on the div containing the text response.

--- my-project/src/components/AboutMePage/AboutMePage.jsx
+++ my-project/src/components/AboutMePage/AboutMePage.jsx
@@ -2,23 +2,34 @@ import React from 'react'
 import { useState } from 'react'
 
 export default function AboutMePage() {
-    //Use state for toggling talking options:
-    const [talkingOptions, setTalkingOptions] = useState(false)
+    //Use useState for toggling talking options:
+    const [questionsVisable, setQuestionsVisable] = useState(true)
 
-    //Usestate to keep track of which option was chosen:
+    //Use useState to keep track of which option was chosen:
     const [selectedQuestion, setSelectedQuestion] = useState(0)
+
+    //Conditionally render question options based on the questionsVisable useState:
+    function toggleQuestionsVisable() {
+        return questionsVisable ?
+            <div className='bg-white w-40 h-36 ml-90 mt-40 absolute flex flex-col justify-center gap-1 items-center border border-black rounded-md'>
+                <button className='text-option' id='option1' onClick={e => { setSelectedQuestion(`${e.target.id}`); setQuestionsVisable(false) }}>Who are you?</button>
+                <button className='text-option' id='option2' onClick={e => { setSelectedQuestion(`${e.target.id}`); setQuestionsVisable(false) }}>Where am I?</button>
+                <button className='text-option' id='option3' onClick={e => { setSelectedQuestion(`${e.target.id}`); setQuestionsVisable(false) }}>What do you do for fun?</button>
+            </div> :
+            <></>;
+    }
 
     //Conditionally render text based on selectedQuestion useState:
     function conditionallyRenderText() {
         switch (selectedQuestion) {
-            case 'option1': 
-            return <p onClick={() => {setSelectedQuestion('')}} className='relative bg-green-400 left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md'>Option1</p>
-            case 'option2': 
-            return <p onClick={() => {setSelectedQuestion('')}} className='relative bg-green-400 left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md'>Option2</p>
-            case 'option3': 
-            return <p onClick={() => {setSelectedQuestion('')}} className='relative bg-green-400 left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md'>Option3</p>
+            case 'option1':
+                return <p onClick={() => { setSelectedQuestion(''); setQuestionsVisable(true) }} className='relative bg-green-400 left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md'>My name is Heng Yang, I'm a software developer who enjoys creating games, making music and chilling on this island.</p>
+            case 'option2':
+                return <p onClick={() => { setSelectedQuestion(''); setQuestionsVisable(true) }} className='relative bg-green-400 left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md'>You are on Paradise Island, a place created from my imagination. You are expereiencing a piece of my mind!</p>
+            case 'option3':
+                return <p onClick={() => { setSelectedQuestion(''); setQuestionsVisable(true) }} className='relative bg-green-400 left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md'>I enjoy making music, 3d modeling, coding, and spending time with friends and family.</p>
             default:
-            return <p onClick={() => {setSelectedQuestion('')}} className='relative bg-green-400 left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md'></p>
+                return <p onClick={() => { setSelectedQuestion(''); setQuestionsVisable(true) }} className='relative bg-green-400 left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md'>You seem a little lost stranger, is there something I can help you with?</p>
         }
 
     }
@@ -27,11 +38,7 @@ export default function AboutMePage() {
     //Div for displaying text response from npc:
     return (
         <div className='absolute top-0 left-0 w-full h-full pt-16'>
-            <div className='bg-white w-40 h-36 ml-90 mt-40 absolute flex flex-col justify-center gap-1 items-center border border-black rounded-md'>
-                <button className='text-option' id='option1' onClick={e => {setSelectedQuestion(`${e.target.id}`)}}>Who are you?</button>
-                <button className='text-option' id='option2' onClick={e => {setSelectedQuestion(`${e.target.id}`)}}>Where am I?</button>
-                <button className='text-option' id='option3' onClick={e => {setSelectedQuestion(`${e.target.id}`)}}>What do you do for fun?</button>
-            </div>
+            {toggleQuestionsVisable()}
             <div className='relative w-3/4 h-44 bg-white ml-40 mt-80 border border-black rounded-md flex flex-row justify-center gap-2'>
                 <div className='left-.5 top-1.5 h-32 w-32 relative'>
                     <img src="../../../public/images/hengs_profile_picture.jpeg" alt="" className='relative border border-black rounded-md' />
