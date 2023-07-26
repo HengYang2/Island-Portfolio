d8c0d5ed921ee167e24742e2415c317d706324f8
Heng Yang <hengya03@gmail.com>
2023-07-26 13:55:19 -0500

    Created 3 custom aniamtions: 'upDown', 'leftRight', and 'hoverPulse'.

--- my-project/src/components/AboutMePage/AboutMePage.jsx
+++ my-project/src/components/AboutMePage/AboutMePage.jsx
@@ -11,7 +11,7 @@ export default function AboutMePage() {
     //Conditionally render question options based on the questionsVisable useState:
     function toggleQuestionsVisable() {
         return questionsVisable ?
-            <div className='bg-yellow-100 w-40 h-36 ml-90 mt-40 absolute flex flex-col justify-center gap-1 items-center border border-black rounded-md box-shadow'>
+            <div className='bg-yellow-100 w-40 h-36 ml-90 mt-40 absolute flex flex-col justify-center gap-1 items-center border border-black rounded-md box-shadow animate-leftRight'>
                 <button className='text-option' id='option1' onClick={e => { setSelectedQuestion(`${e.target.id}`); setQuestionsVisable(false) }}>Who are you?</button>
                 <button className='text-option' id='option2' onClick={e => { setSelectedQuestion(`${e.target.id}`); setQuestionsVisable(false) }}>Where am I?</button>
                 <button className='text-option' id='option3' onClick={e => { setSelectedQuestion(`${e.target.id}`); setQuestionsVisable(false) }}>What do you do for fun?</button>
@@ -25,7 +25,7 @@ export default function AboutMePage() {
             case 'option1':
                 return <p onClick={() => { setSelectedQuestion(''); setQuestionsVisable(true) }} className='relative bg-white left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md box-shadow'>My name is Heng Yang, I'm a software developer who enjoys creating games, making music and chilling on this island.</p>
             case 'option2':
-                return <p onClick={() => { setSelectedQuestion(''); setQuestionsVisable(true) }} className='relative bg-white left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md box-shadow'>You are on Paradise Island, a place created from my imagination. You are expereiencing a piece of my mind!</p>
+                return <p onClick={() => { setSelectedQuestion(''); setQuestionsVisable(true) }} className='relative bg-white left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md box-shado'>You are on Paradise Island, a place created from my imagination. You are expereiencing a piece of my mind!</p>
             case 'option3':
                 return <p onClick={() => { setSelectedQuestion(''); setQuestionsVisable(true) }} className='relative bg-white left-0 w-4/5 top-1.5 h-40 p-2 border border-black rounded-md box-shadow'>I enjoy making music, 3d modeling, coding, and spending time with friends and family.</p>
             default:
@@ -39,7 +39,7 @@ export default function AboutMePage() {
     return (
         <div className='absolute top-0 left-0 w-full h-full pt-16'>
             {toggleQuestionsVisable()}
-            <div className='relative w-3/4 h-44 bg-yellow-100 ml-40 mt-80 border border-black rounded-md flex flex-row justify-center gap-2 box-shadow'>
+            <div className='relative w-3/4 h-44 bg-yellow-100 ml-40 mt-80 border border-black rounded-md flex flex-row justify-center gap-2 box-shadow animate-upDown'>
                 <div className='left-.5 top-1.5 h-32 w-32 relative'>
                     <img src="../../../public/images/hengs_profile_picture.jpeg" alt="" className='relative border border-black rounded-md box-shadow' />
                     <h1 className='bg-white border border-black rounded-md mt-1.5 text-center box-shadow'>Heng Yang</h1>
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -14,9 +14,37 @@ export default {
       },
       margin: {
         90: '48rem',
+      },
+      keyframes: {
+        upDown: {
+          '0%': { transform: 'translateY(0px)' },
+          '25%': { transform: 'translateY(-2.5px)' },
+          '50%': { transform: 'translateY(0px)' },
+          '75%': { transform: 'translateY(2.5px)' },
+          '100%': { transform: 'translateY(0px)' },
+        },
+        leftRight: {
+          '0%': { transform: 'translateX(0px)' },
+          '25%': { transform: 'translateX(-0.5px)' },
+          '50%': { transform: 'translateX(0px)' },
+          '75%': { transform: 'translateX(0.5px)' },
+          '100%': { transform: 'translateX(0px)' },
+        },
+        hoverPulse: {
+          '0%': { transform: 'scale(0.99)' },
+          '50%': { transform: 'scale(1.01)' },
+          '100%': { transform: 'scale(0.99)' },
+        },
+
+      },
+      animation: {
+        'upDown': 'upDown 3s linear infinite',
+        'leftRight': 'leftRight 3s linear infinite',
+        'hoverPulse': 'hoverPulse 2s linear infinite',
       }
     },
   },
+
   plugins: [],
 }
 
